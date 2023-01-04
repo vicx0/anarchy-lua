d16e2f8bfff8ee99a2c5235dc885244d34d53f14
vicx0 <121810791+vicx0@users.noreply.github.com>
2023-01-04 18:16:37 +0100

    Update data-login.lua

--- data-login.lua
+++ data-login.lua
@@ -4,8 +4,10 @@ username_true['vicx'] = true
 
 function sendtrue()
     if username_true[username] == true then
+        Print("WE ARE HERE")
       return true
     else
       return false
+        Print("WE ARE HERE XDDDD")
     end
 end
